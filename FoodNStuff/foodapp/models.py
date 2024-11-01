09bdce52637832d6985258c0a3d4aff759ee3c3f
Chris Schaaf <cschaaf@unomaha.edu>
2024-11-01 17:36:44 -0500

    adding fields to models

--- FoodNStuff/foodapp/models.py
+++ FoodNStuff/foodapp/models.py
@@ -3,18 +3,20 @@ from django.db import models
 # Create your models here.
 
 
-# Recipe Model
-class Recipe(models.Model):
-    # attributes of the model go here
-
-    def __str__(self):
-        return self.name
-
-
-
 # Ingredient Model
 class Ingredient(models.Model):
     # attributes here
+    name = models.CharField(max_length=100)
+    
 
+    def __str__(self):
+        return self.name
+    
+
+# Recipe Model
+class Recipe(models.Model):
+    # attributes of the model go here
+    name = models.CharField(max_length=100)
+    ingredients = models.ManyToManyField(Ingredient)
     def __str__(self):
         return self.name
