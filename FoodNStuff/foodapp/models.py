9e75974018dadb75b381df4b224537257b46c580
Chris Schaaf <cschaaf@unomaha.edu>
2024-11-07 09:32:45 -0600

    added attributes to Ingredient model

--- FoodNStuff/foodapp/models.py
+++ FoodNStuff/foodapp/models.py
@@ -8,6 +8,11 @@ from django.contrib.auth.models import User
 class Ingredient(models.Model):
     # attributes here
     name = models.CharField(max_length=100)
+    description = models.TextField(null=True)
+    quantity = models.DecimalField(max_digits=8, decimal_places=2)
+    measurement_unit = models.CharField(max_length=50)
+    # CASCADE --> deletes references to an instance when it is removed
+    recipe = models.ForeignKey('Recipe', on_delete=models.CASCADE, related_name='ingredients')
     
 
     def __str__(self):
@@ -20,10 +25,13 @@ class Recipe(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     servings = models.PositiveIntegerField()
+    # prep time in minutes, allow nulls if you don't want to enter this
+    prep_time = models.PositiveIntegerField(null=True)
+    # cook time in minutes, allow nulls
+    cook_time = models.PositiveIntegerField(null=True)
     ingredients = models.ManyToManyField(Ingredient)
     protein = models.ForeignKey('Protein', on_delete=models.SET_NULL, null=True)
     category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
-
     image = models.ImageField(upload_to='recipes/', blank=True, null=True)
 
     def __str__(self):
@@ -45,3 +53,5 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+    
+# NOTE:  maybe add a model for Nutritional information that can be populated by calling the EDAMAM API and then stored
