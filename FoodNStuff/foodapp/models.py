580b0ddcfaed6942f3e50e3189af12ddf2f5c001
Chris Schaaf <cschaaf@unomaha.edu>
2024-11-07 09:19:49 -0600

    Added models for protein and category, adding attributes to recipe model

--- FoodNStuff/foodapp/models.py
+++ FoodNStuff/foodapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -17,6 +18,30 @@ class Ingredient(models.Model):
 class Recipe(models.Model):
     # attributes of the model go here
     name = models.CharField(max_length=100)
+    description = models.TextField()
+    servings = models.PositiveIntegerField()
     ingredients = models.ManyToManyField(Ingredient)
+    protein = models.ForeignKey('Protein', on_delete=models.SET_NULL, null=True)
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
+
+    image = models.ImageField(upload_to='recipes/', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+# Model for Protein type (e.g. Poultry, Beef, Pork, Seafood, Vegetable, etc)   
+class Protein(models.Model):
+    # attributes
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+# Model for meal type (e.g. Breakfast, Lunch, Dinner, Soup, Salad, Sandwich, etc)
+class Category(models.Model):
+    # attributes
+    name = models.CharField(max_length=100)
+
     def __str__(self):
         return self.name
