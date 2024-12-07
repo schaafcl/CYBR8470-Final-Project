dbe069f2af8a240a986ed0f40f53fd39cbf91800
Chris Schaaf <cschaaf@unomaha.edu>
2024-12-07 16:51:32 -0600

    Backend changes to allow searching by recipe by name

--- Backend/FoodNStuff/foodapp/views.py
+++ Backend/FoodNStuff/foodapp/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 #from rest_framework.decorators import api_view
 from .models import Ingredient, Recipe
 from .serializers import IngredientSerializer, RecipeSerializer
-from rest_framework import status, permissions, renderers, viewsets
+from rest_framework import status, permissions, renderers, viewsets, filters
 #from rest_framework.decorators import api_view, permission_classes, renderer_classes
 #from django.contrib.auth import authenticate
 #from spyne import Application, rpc, ServiceBase, Integer, Unicode
@@ -14,15 +14,21 @@ from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
-# imported from previous labs and commented out things not needed, will clearn up later
+# imported from previous labs and commented out things not needed, will clean up later
+
 class IngredientViewSet(viewsets.ModelViewSet):
     queryset = Ingredient.objects.all()
     serializer_class = IngredientSerializer
+    
+    
 
-
+# 12-7 adding search_fields to allow searching by recipe name
 class RecipeViewSet(viewsets.ModelViewSet):
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
+    filter_backends = [filters.SearchFilter]
+    # enables the attribute recipe name to be searchable via user input
+    search_fields = ['name']
 
     def perform_create(self, serializer):
         serializer.save()
@@ -31,3 +37,11 @@ class RecipeViewSet(viewsets.ModelViewSet):
         #handles posts to create a new recipe
         #individual ingredietns handled in the serializer
         return super().create(request, *args, **kwargs)
+    
+    # used to search for recipe by its name, case insensitive
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        search_term = self.request.query_params.get('search', None)
+        if search_term:
+            queryset = queryset.filter(name__icontains=search_term)  # Case-insensitive search on title
+        return queryset
