993e82054ef3598c586d45aa99333cac02b514e9
Chris Schaaf <cschaaf@unomaha.edu>
2024-12-07 16:54:02 -0600

    initially adding of a component for a search bar for recipes

---
+++ Frontend/final-client/src/components/RecipeSearchBar.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+const SearchBar = ({ onSearch }) => {
+  const [query, setQuery] = useState('');
+
+  const handleChange = (e) => {
+    setQuery(e.target.value);
+  };
+
+  const handleSearch = () => {
+    onSearch(query);
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Search for recipes"
+        value={query}
+        onChange={handleChange}
+      />
+      <button onClick={handleSearch}>Search</button>
+    </div>
+  );
+};
+
+export default SearchBar;
