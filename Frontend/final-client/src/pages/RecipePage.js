84a65969dd398791c01f117d480cd8697a7077ae
Chris Schaaf <cschaaf@unomaha.edu>
2024-12-08 16:13:35 -0600

    Adding functionality to allow deleting individual recipes from their page

--- Frontend/final-client/src/pages/RecipePage.js
+++ Frontend/final-client/src/pages/RecipePage.js
@@ -1,15 +1,21 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 const RecipePage = () => {
   const { id } = useParams();  // Extract the recipe id from the URL
   const [recipe, setRecipe] = useState(null);
+  // navigate back after deletion
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchRecipe = async () => {
       try {
         const response = await fetch(`http://localhost:8000/api/recipes/${id}/`);
+        if(!response.ok) {
+          throw new Error('Recipe was not found!');
+        }
         const data = await response.json();
+        // print for testing
         console.log(data)
         setRecipe(data);
       } catch (error) {
@@ -20,12 +26,36 @@ const RecipePage = () => {
     fetchRecipe();
   }, [id]);
 
+  // Handles deleting of recipe.  Triggered via button on the recipe page, sends a delete request to the api to remove the recipe and includes a confirmation request
+  const handleDelete = async () => {
+    const confirmed = window.confirm("Are you sure you want to delete this recipe?");
+    if (confirmed) {
+      try {
+        const response = await fetch(`http://localhost:8000/api/recipes/${id}/`, {
+          method: 'DELETE',
+        });
+
+        if (response.ok) {
+          alert('Recipe deleted successfully!');
+          // redirect back to search results after deleting recipe
+          navigate(-1);  
+        } else {
+          alert('Failed to delete the recipe');
+        }
+      } catch (error) {
+        console.error('Error deleting recipe:', error);
+        alert('An error occurred while deleting the recipe');
+      }
+    }
+  };
+
   if (!recipe) {
     return <div>Loading...</div>;
   }
 
   return (
     <div>
+      {/* displays information about recipe and maps ingredients ot list items */}
       <h2>{recipe.name}</h2>
       <p>{recipe.description}</p>
       <h3>Ingredients:</h3>
@@ -36,9 +66,10 @@ const RecipePage = () => {
           </li>
         ))}
       </ul>
-      
       <h3>Instructions:</h3>
       <p>{recipe.instructions}</p>
+      {/* button to remove recipe */}
+      <button onClick={handleDelete}>Delete Recipe</button>
     </div>
   );
 };
