df90bad65570e3db80bcd85461f03c51a40708d5
Chris Schaaf <cschaaf@unomaha.edu>
2024-12-12 14:10:38 -0600

    adding details to the about page for completeness

--- Frontend/final-client/src/pages/AboutPage.js
+++ Frontend/final-client/src/pages/AboutPage.js
@@ -3,8 +3,14 @@ import React from 'react';
 const AboutPage = () => {
   return (
     <div>
-      <h2>About Us</h2>
-      <p>This is an about page for the recipe app.</p>
+      <h2>About Me</h2>
+      <p>This application was developed by Chris Schaaf, for the purposes of CYBR8470 Secure Web App Development's final project</p>
+      
+      <p>This application works as a recipe book to store recipes and allow users to search for recipes by a variety of criteria.  It was meant to address a certain cabinet problem that I imgaine others also experience.  I
+        refer to that cabinet that holds stacked cookbooks, overstuffed manilla folders, and piles of loose pages stuffed in a cabinet holding your own as well as your extended family's extensive culinary history.  A user can
+        consolidate their many recipes in a locally hosted database, add to them over time, and browse through them based on what's on sale for a given week or what "just sounds good right now".
+      </p>
+      <p><b>Chris Schaaf<br></br>M.S. Cyber Security Operations <br></br>University of Nebraska at Omaha <br></br>cschaaf@unomaha.edu</b></p>
     </div>
   );
 };
