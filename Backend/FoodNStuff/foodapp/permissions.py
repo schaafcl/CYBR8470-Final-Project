9c8f6c60b190b50bb47d69b4ea32ade59a1d8406
Chris Schaaf <cschaaf@unomaha.edu>
2024-12-15 16:35:27 -0600

    permissions and authentication working, staff/admin users have access to all models, users only have access to the ones they own

--- Backend/FoodNStuff/foodapp/permissions.py
+++ Backend/FoodNStuff/foodapp/permissions.py
@@ -1,10 +1,15 @@
 from rest_framework import permissions
 
 # imported from webservice security lab
-class IsOwner(permissions.BasePermission):
-    """
-    Custom permission to only allow owners of an object to view it.
-    """
+class IsOwnerOrStaff(permissions.BasePermission):
+    
+    # Custom permission to only allow owners of an object to view it.
+    
 
     def has_object_permission(self, request, view, obj):
+        # allow access if a staff member
+        if request.user.is_staff:
+            return True
+        # else return true if the user is the object owner
         return obj.recipe_owner == request.user
+    
--- Backend/FoodNStuff/foodapp/views.py
+++ Backend/FoodNStuff/foodapp/views.py
@@ -5,8 +5,7 @@ from .serializers import RecipeSerializer
 from rest_framework import status, permissions, renderers, viewsets, filters
 from rest_framework.response import Response
 from rest_framework.decorators import action
-from .permissions import IsOwner
-# from django.views.decorators.csrf import csrf_exempt
+from .permissions import IsOwnerOrStaff
 
 
 # Viewset for recipes
@@ -17,17 +16,23 @@ class RecipeViewSet(viewsets.ModelViewSet):
     filter_backends = [DjangoFilterBackend, filters.SearchFilter]
     filterset_fields = ["name", "protein", "category"]
     search_fields = ["name", "protein", "category"]
-    permission_classes = [permissions.IsAuthenticated, IsOwner]
+    permission_classes = [permissions.IsAuthenticated, IsOwnerOrStaff]
 
 
     def retrieve(self, request, *args, **kwargs):
 
         recipe = self.get_object()
-        if recipe.recipe_owner != request.user:
-            return Response({'error': 'You do not have permission to view this dog.'}, status=403)
-
         serializer = self.get_serializer(recipe)
         return Response(serializer.data)
+        '''
+        if request.user.is_staff:
+            serializer = self.get_serializer(recipe)
+            return Response(serializer.data)
+        if recipe.recipe_owner != request.user:
+            return Response({'error': 'You do not have permission to view this dog.'}, status=403)
+        serializer = self.get_serializer(recipe)
+        return Response(serializer.data)
+        '''
     
 
     def perform_create(self, serializer):
@@ -35,5 +40,8 @@ class RecipeViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         user = self.request.user
-        return Recipe.objects.filter(recipe_owner = user)
+        if user.is_staff:
+            return Recipe.objects
+        else:
+            return Recipe.objects.filter(recipe_owner = user)
     
